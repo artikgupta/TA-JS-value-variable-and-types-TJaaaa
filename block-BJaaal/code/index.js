05b17e547ee39fd39e4d69f62537a1f7b9d74a8d
artikgupta <artig9262@gmail.com>
2020-11-16 11:48:58 +0530

    refactored the code

--- block-BJaaal/code/index.js
+++ block-BJaaal/code/index.js
@@ -17,8 +17,8 @@ let lastName = "Stark";
   - Alert the message `I'm Arya Stark of Winterfell` using the variable name firstName and lastName.
 */
 
-alert("I" + "am" + username + "and" + "my" + "bio" + "is" + bio);
-alert("I" + "am" + firstName + lastName + "of" + "Winterfell");
+alert("I  am" + username + "and my  bio is" + bio);
+alert("I'm" + firstName + lastName + "of Winterfell");
 
 /* Do the following using string template literal
   - Alert message saying `I am [username] and my bio is [bio]`. Replace square brackets with the value of variable.
@@ -26,7 +26,7 @@ alert("I" + "am" + firstName + lastName + "of" + "Winterfell");
 */
 
 alert(`I am ${username}  and my bio is ${bio} `);
-alert(`I am ${firstName} ${lastName} of Winterfell`);
+alert(`I'm ${firstName} ${lastName} of Winterfell`);
 
 let amount = 200;
 const taxPercent = 10;
@@ -38,8 +38,11 @@ const taxPercent = 10;
   - Above message should be in two lines
 */
 
-alert(`The total amount is ${amount}`);
-alert(`the tax amount is (${amount} * ${taxPercent}) / 100]`);
+alert(
+  `The total amount is ${amount}  the tax amount is ${
+    (amount * taxPercent) / 100
+  }`
+);
 
 /* Use double quote and escape character:
   - Alert a message saying `The total amount is [amount]
@@ -48,5 +51,15 @@ alert(`the tax amount is (${amount} * ${taxPercent}) / 100]`);
   - Above message should be in two lines
 */
 
-alert("The total amount is '\n amount'");
-alert("the tax amount is '[(amount * taxPercent) / 100]'");
+// alert(
+//   "The total amount is" +
+//     amount +
+//     "\n the tax amount is"
+// );
+
+alert(
+  "The total amount is" +
+    amount +
+    "\n  the tax amount is " +
+    (amount * taxPercent) / 100
+);
